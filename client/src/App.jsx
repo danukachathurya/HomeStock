1d9c255e0e0a7d77163ae82201234a8ab076e707
Danuka Chathurya <danukachathurya8@gmail.com>
2025-03-10 18:58:42 +0530

    Connect to the database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/Signin';
 import Header from './components/Header';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
